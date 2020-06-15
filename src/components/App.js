81ef455bf745e43c0b498e07af57dc7236751efa
Aldo <aldo.rojas@ecloudsolutions.com>
2020-06-14 22:37:34 -0300

    initial commit

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import PostList from './PostList';
+
+const App = () => {
+    return (
+        <div className="ui container">
+            App
+            <PostList/>
+        </div>
+    );
+};
+
+export default App;
